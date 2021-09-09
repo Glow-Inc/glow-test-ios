a75012db8f01a97bf9b55dc5b22c537655dd25b6
Eric <xuqifeng@gmail.com>
2021-09-09 12:28:11 +0800

    glow ios test project

---
+++ HomeVcTest/HomeVc/ChildViewControllers.swift
@@ -0,0 +1,21 @@
+//
+//  ChildViewControllers.swift
+//  HomeVc
+//
+//  Created by Eric Xu on 2021/9/8.
+//
+
+import Foundation
+import UIKit
+
+class TodayViewController: UITableViewController {
+
+}
+
+class AnalysisViewController: UITableViewController {
+
+}
+
+class ResourcesViewController: UITableViewController {
+
+}
---
+++ HomeVcTest/HomeVcTests/HomeVcTests.swift
@@ -0,0 +1,33 @@
+//
+//  HomeVcTests.swift
+//  HomeVcTests
+//
+//  Created by Eric Xu on 2021/9/8.
+//
+
+import XCTest
+@testable import HomeVc
+
+class HomeVcTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
